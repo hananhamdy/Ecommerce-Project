8b421a6c56224523f09b61efcb115f337f67c855
HananHamdy <HananHamdy@blackstoneeit.com>
2024-11-13 20:00:07 +0200

    oops design

--- src/shared-module/components/oops/oops.component.ts
+++ src/shared-module/components/oops/oops.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-oops',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './oops.component.html',
   styleUrl: './oops.component.scss'
 })
